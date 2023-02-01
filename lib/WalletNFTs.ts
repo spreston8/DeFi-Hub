fdff8d950e376abd69b6db69f097030da306ca92
Stephen Preston <stevepreston747@gmail.com>
2023-01-31 17:00:14 -0800

    update getWalletNFTs

--- lib/WalletNFTs.ts
+++ lib/WalletNFTs.ts
@@ -18,6 +18,7 @@ export async function getWalletNfts(walletAddress: string, chainIdHex: number) {
   });
 
   const nfts = initialMoralisResponse.toJSON();
+  console.log(nfts);
 
   // if there are owned nfts, add to collection
   if (nfts.result) {
@@ -60,111 +61,68 @@ export async function getWalletNfts(walletAddress: string, chainIdHex: number) {
   }
 
   // // if there are more than 100 total nfts, fetch the rest and add to collection
-  // const totalNfts = moralisResponse.pagination.total || 0;
-  // const pageCount = Math.floor(totalNfts / 100);
-  // let cursor = moralisResponse.pagination.cursor;
+  const totalNfts = nfts.total || 0;
+  const pageCount = Math.floor(totalNfts / 100);
+  let cursor = nfts.cursor;
 
-  // if (pageCount > 0) {
-  //   for (let i = 0; i < pageCount; i++) {
-  //     moralisResponse = await Moralis.EvmApi.nft.getWalletNFTs({
-  //       address: userAddress,
-  //       token_addresses: [contractAddress],
-  //       chain: chainIdHex,
-  //       limit: 100,
-  //       cursor: cursor,
-  //     });
+  if (pageCount > 0) {
+    for (let i = 0; i < pageCount; i++) {
+      const moralisResponse = await Moralis.EvmApi.nft.getWalletNFTs({
+        address: walletAddress,
+        chain: chainIdHex,
+        cursor: cursor,
+      });
 
-  //     const nfts = moralisResponse.toJSON();
-  //     if (nfts) {
-  //       for (let i = 0; i < nfts.length; i++) {
-  //         mRcontractAddress = nfts[i].tokenAddress;
-  //         token_id = Number(nfts[i].tokenId);
-  //         tokenMetadata = await getTokenMetadata(token_id);
+      const nfts = moralisResponse.toJSON();
+      if (nfts.result) {
+        for (let i = 0; i < nfts.result.length; i++) {
+          const currNFT = nfts.result[i];
+          const currNFTMetadata: NFTMetadata = {
+            name: '',
+            description: '',
+            image: '',
+            attributes: {},
+            token_id: '',
+            token_hash: '',
+            contract_type: '',
+            contract_name: '',
+            token_address: '',
+            symbol: '',
+          };
 
-  //         const blockNumberMinted = nfts[i].blockNumberMinted || '';
-  //         const timestamp = (
-  //           await web3Provider.getBlock(Number(blockNumberMinted))
-  //         ).timestamp;
+          currNFTMetadata.token_id = currNFT.token_id;
+          currNFTMetadata.token_hash = currNFT.token_hash;
+          currNFTMetadata.contract_type = currNFT.contract_type;
+          currNFTMetadata.contract_name = currNFT.name;
+          currNFTMetadata.token_address = currNFT.token_address;
+          currNFTMetadata.symbol = currNFT.symbol;
 
-  //         if (chainIdHex === '0x38') {
-  //           metadata = {
-  //             name: tokenMetadata.name,
-  //             description: tokenMetadata.description,
-  //             image: tokenMetadata.image.replace(
-  //               'ipfs://',
-  //               'https://ipfs.io/ipfs/'
-  //             ), // ToDo: this needs paramaterization
-  //             attributes: tokenMetadata.attributes,
-  //             contractAddress: mRcontractAddress,
-  //             tokenId: token_id,
-  //             tokenID: token_id,
-  //             token_id: token_id,
-  //             contractStandard: 'BEP-1155', // moralis returns ERC-1155
-  //             mintTimestamp: timestamp,
-  //           };
-  //         } else {
-  //           metadata = {
-  //             name: 'test nft',
-  //             description: 'test nft description',
-  //             image: '/static/img/logo.svg',
-  //             attributes: { object: {} },
-  //             contractAddress: mRcontractAddress,
-  //             tokenId: token_id,
-  //             tokenID: token_id,
-  //             token_id: token_id,
-  //             contractStandard: 'BEP-1155', // moralis returns ERC-1155
-  //             mintTimestamp: timestamp,
-  //           };
-  //         }
+          if (currNFT.metadata) {
+            const jsonDescription = JSON.parse(currNFT.metadata);
+            currNFTMetadata.name = jsonDescription.name;
+            currNFTMetadata.description = jsonDescription.description;
+            currNFTMetadata.image = jsonDescription.image;
+            currNFTMetadata.attributes = jsonDescription.attributes;
+          } else {
+            currNFTMetadata.name = 'nft name not provided';
+            currNFTMetadata.description = 'description not provided';
+            currNFTMetadata.attributes = 'attributes not provided';
+          }
 
-  //         // make sure we don't push the same nft
-  //         if (!NFT_COLLECTION.includes(metadata)) {
-  //           NFT_COLLECTION.push(metadata);
-  //         } else {
-  //           console.log(`Token ID: ${token_id} already in NFT COLLECTION`);
-  //         }
-  //       }
-  //     }
+          // make sure we don't push the same nft
+          if (!NFT_COLLECTION.includes(currNFTMetadata)) {
+            NFT_COLLECTION.push(currNFTMetadata);
+          } else {
+            console.log(
+              `Token ID: ${currNFT.token_id} already in NFT COLLECTION`
+            );
+          }
+        }
+      }
 
-  //     cursor = moralisResponse.pagination.cursor;
-  //   }
-  // }
+      cursor = moralisResponse.pagination.cursor;
+    }
+  }
 
   return NFT_COLLECTION;
 }
-
-// export async function getWalletNftCollections(
-//   web3Provider: ethers.providers.Web3Provider,
-//   chainID: number
-// ) {
-//   const NFT_COLLECTION: NFTMetadata[] = [];
-
-//   switch (chainID) {
-//     case 1:
-//       // ETH MAINNET
-//       return NFT_COLLECTION;
-//     case 5:
-//       // ETH GOERLI
-//       return NFT_COLLECTION;
-//     case 56:
-//       // BSC MAINNET
-//       NFT_COLLECTION = await getNftCollectionBSC(
-//         web3Provider,
-//         userAddress,
-//         contractAddress,
-//         '0x38'
-//       );
-//       return NFT_COLLECTION;
-//     case 97:
-//       //BSC TESTNET
-//       NFT_COLLECTION = await getNftCollectionBSC(
-//         web3Provider,
-//         userAddress,
-//         contractAddress,
-//         '0x61'
-//       );
-//       return NFT_COLLECTION;
-//     default:
-//       return NFT_COLLECTION;
-//   }
-// }
